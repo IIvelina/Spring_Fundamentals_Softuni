1d092be17fb83e8e2bed4dd8dab075e9c5aac1bb
Ivelina Ivanova <125279068+IIvelina@users.noreply.github.com>
2024-05-26 17:15:18 +0300

    Delete mobilele directory

--- mobilele/src/main/java/bg/softuni/mobilele/service/impl/OfferServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package bg.softuni.mobilele.service.impl;
-
-import bg.softuni.mobilele.service.OfferService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class OfferServiceImpl implements OfferService {
-}
